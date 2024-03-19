01e8a8fc9eaf520e4334b3aab01760ea14821a83
KevJer <cl.castro@moneyblinks.com>
2024-03-19 09:03:35 -0500

    reto  2 por hacer

--- reto2.py
+++ reto2.py
@@ -1,1 +1,13 @@
-print("HOLA")
+import random
+
+aleatorios = random.randint(1,9)  
+aleatorios_dos = random.randint(1,9)
+
+if aleatorios == 4:
+    print("Te ganaste un chupete")
+elif aleatorios == 8:
+    print("Te ganaste un balon")
+elif aleatorios == 3 and aleatorios_dos == 7:
+    print("Te ganaste un televisor")
+else:
+    print("Perdiste!!")       
